173e2dcf0b0938e207c610bab9e8d7552b720bd6
Dimty <nikitin-dmitriyy@mail.ru>
2022-09-07 13:04:24 +0600

    UnitTests

---
+++ Geometry/UnitTests.cs
@@ -0,0 +1,47 @@
+﻿using Geometry;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeometryTest;
+
+[TestClass]
+public class GeometryLibTest
+{
+    [TestMethod]
+    public void TriangleAreaTest()
+    {
+        double a = 3.0d;
+        double b = 4.0d;
+        double c = 5.0d;
+        double expected = 6;
+
+        Shape triangle = new Triangle(a, b, c);
+        var actual = triangle.GetArea();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void CircleAreaTest()
+    {
+        double r = 1.0d;
+        double expected = Math.PI;
+
+        Shape triangle = new Circle(r);
+        var actual = triangle.GetArea();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void TriangleIsRectangularTest()
+    {
+        double a = 3.0d;
+        double b = 4.0d;
+        double c = 5.0d;
+
+        ITypeOfTriangle triangle = new Triangle(a, b, c);
+        var actual = triangle.IsRectangular();
+
+        Assert.IsTrue(actual);
+    }
+}
